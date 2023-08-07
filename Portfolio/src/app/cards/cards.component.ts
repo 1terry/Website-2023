4c675551e18ed969ea248e9ba4813058d0073af7
Terry Ju <t.juottawa@gmail.com>
2023-08-07 01:58:13 -0400

    updated cards

--- Portfolio/src/app/cards/cards.component.ts
+++ Portfolio/src/app/cards/cards.component.ts
@@ -1,31 +1,40 @@
 import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 
+
 @Component({
   selector: 'app-cards',
   templateUrl: './cards.component.html',
-  styleUrls: ['./cards.component.scss']
-})
-export class CardsComponent implements OnInit {
-  @ViewChild('card1') card1: ElementRef;
+  styleUrls: ['./cards.component.scss'],
 
+
+})
+
+
+export class CardsComponent implements OnInit {
+   @ViewChild('card1') card1: ElementRef;
+   @ViewChild('card2') card2: ElementRef;
+   @ViewChild('card3') card3: ElementRef;
+ 
   constructor() { }
 
   ngOnInit(): void {
+    
   }
 
-  // $(".flip").click(function(){
-  //   $(this).parents(".card").toggleClass("flipped");
-  // });
-  // $(".clickcard").click(function(){
-  //   $(this).toggleClass("flipped");
-  // });
+  flipCard(selected: ElementRef){
+    selected.nativeElement.classList.toggle('is-flipped');
+  }
 
-  // flipCard(selected: ElementRef) {
-  //   selected.nativeElement.parents(".card").toggleClass("flipped");
-  // }
+  flipCard1(){
+    this.flipCard(this.card1);
+  }
 
-  // changeCard1(){
-  //   this.flipCard(this.card1);
-  // }
+  flipCard2(){
+    this.flipCard(this.card2);
+  }
+
+  flipCard3(){
+    this.flipCard(this.card3);
+  }
 
 }
